f82aafefe8c2879e87d11fde18b22e4b22fd1bfd
violeta <violetadefreitas@gmail.com>
2022-12-27 23:32:15 -0300

    quarto

--- herding.py
+++ herding.py
@@ -7,12 +7,15 @@ from matplotlib.animation import PillowWriter
 
 """
 
-Implemeting the herding model from https://royalsocietypublishing.org/doi/full/10.1098/rsif.2014.0719
+Implementing the herding model from https://royalsocietypublishing.org/doi/full/10.1098/rsif.2014.0719
+
+Notes:
+(1) I'm using the parameters ra and deltaS bigger than in the parameter table from the article to make faster and better looking animation; with the table values the model still can succeed but it takes way longer.
 
 """
 
 class model:
-        def __init__(self,  A, V, S, rs = 65.0, ra = 5.0, h = 0.5, c = 1.05, pa = 2, ps = 1.0, e = 0.3, delta0 = 1, deltaS = 4.5):
+        def __init__(self,  A, V, S, rs = 65.0, ra = 4.0, h = 0.5, c = 1.05, pa = 2, ps = 1.0, e = 0.3, delta0 = 1, deltaS = 3.8):
             self.A= np.float64(A)
             self.V = np.float64(V)
             self.S= np.float64(S)
@@ -63,56 +66,62 @@ class model:
             #making the shepherd chill if they're too close to an agent
             if any(distances_shepherd < (3*self.ra)):
                 return model(self.A,self.V,self.S)
-            #collecting
+            #Collecting
             if np.any(distances_gcm > self.dn):
-                print('Collecting')
+                #print('Collecting')
                 runaway_index = np.argmax(distances_gcm)
                 runaway = self.A[runaway_index]
                 Pc = runaway + (3*self.ra) * model.normalize(runaway - self.GCM)
                 self.S = self.S + self.deltaS * model.normalize(Pc - self.S)
                 return model(self.A,self.V,self.S)
-            #herding
+            #Herding
             else:
-                print('Herding')
+                #print('Herding')
                 Pd = self.GCM + self.ra*sqrt(self.N)*model.normalize(self.GCM)
                 self.S = self.S + self.deltaS * model.normalize(Pd - self.S)
                 return model(self.A,self.V,self.S)
         def updateAll(self):
             for i in range(self.N): self = self.updateAgent(i)
             self = self.updateShepherd()
-            if self.goal: print ("Successful shepherding!")
             return self
 
+        def didItSucceed(self,frames):
+            for b in range(frames):
+                self = self.updateAll()
+                if self.goal: return True
+            return False
+
 #Testing and making animations
-L = 150
-n = 10
-x = L*np.random.rand(n)
-y = L*np.random.rand(n)
-coord = np.column_stack((x,y))
-xS = L*np.random.rand(1)
-yS = L*np.random.rand(1)
-coordS = np.column_stack((xS,yS))
+def maketest(L = 150,n = 10):
+    x = L*np.random.rand(n)
+    y = L*np.random.rand(n)
+    coord = np.column_stack((x,y))
+    xS = L*np.random.rand(1)
+    yS = L*np.random.rand(1)
+    coordS = np.column_stack((xS,yS))
 
-aff = np.array(coord)
-bff = np.zeros([2,n]).T + 0.001
-cff = np.array(coordS)
-moo = model(aff,bff,cff)
+    ag = np.array(coord)
+    vl = np.zeros([2,n]).T + 0.001
+    sh = np.array(coordS)
+    moo = model(ag,vl,sh)
+    return moo
 
+def makegif(moo, L = 150, frames = 500):
+    fig = plt.figure()
+    plt.ylim(-2*L,2*L)
+    plt.xlim(-2*L,2*L)
+    plt.axis('off')
 
-fig = plt.figure()
-plt.ylim(-2*L,2*L)
-plt.xlim(-2*L,2*L)
+    agents, = plt.plot(moo.A.T[0],moo.A.T[1],'xb')
+    shepherd, = plt.plot(moo.S.T[0],moo.S.T[1],'or')
+    #showgcm, = plt.plot(moo.GCM.T[0],moo.GCM.T[1], 'og')
+    showOrigin = plt.plot([0],[0], 'og')
 
-
-agents, = plt.plot(moo.A.T[0],moo.A.T[1],'xb')
-shepherd, = plt.plot(moo.S.T[0],moo.S.T[1],'or')
-showgcm, = plt.plot(moo.GCM.T[0],moo.GCM.T[1], 'og')
-
-writer = PillowWriter(fps = 15)
-with writer.saving(fig, "herding.gif", 100):
-    for b in np.linspace(0,1,600):
-        moo = moo.updateAll()
-        agents.set_data(moo.A.T[0],moo.A.T[1])
-        shepherd.set_data(moo.S.T[0],moo.S.T[1])
-        showgcm.set_data(moo.GCM.T[0],moo.GCM.T[1])
-        writer.grab_frame()
+    writer = PillowWriter(fps = 15)
+    with writer.saving(fig, "herding.gif", 100):
+        for b in range(frames):
+            moo = moo.updateAll()
+            agents.set_data(moo.A.T[0],moo.A.T[1])
+            shepherd.set_data(moo.S.T[0],moo.S.T[1])
+            #showgcm.set_data(moo.GCM.T[0],moo.GCM.T[1])
+            writer.grab_frame()
